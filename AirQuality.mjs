f00ae9443a4ddf8759611e595aabb269a3aa5fbf
AndreyYamkovoy1 <100922899+AndreyYamkovoy1@users.noreply.github.com>
2022-11-21 11:01:49 +0000

    Updated AirQuality with historical input
    Can now give historical input in format yyyy/mm/dd hh:mm:ss
    Updated to newer version of node js interpreter so now using 
    async await for the fetch

--- AirQuality.mjs
+++ AirQuality.mjs
@@ -1,68 +1,85 @@
+let lat = [];
+let lon = [];
+let AQI = [];
+let CO = [];
+let NO = [];
+let NO2 = [];
+let OZONE = [];
+let SO2 = [];
+let PM25 = [];
+let PM10 = [];
+let NH3 = [];
+function main(){
+    let inputLat = 10.2;
+    let inputLon = 20.1;
+    let startTime = '2022/11/18 00:00:00';
+    let endTime = '2022/11/19 00:00:00';
 
-// Get air quality for given lat/lon for an hour through unix time
-import fetch from "node-fetch";
-let inputLat = 10.2;
-let inputLon = 20.1;
-let startTime = 1668854358; //Saturday, November 19, 2022 10:39:18 AM
-let endTime =  1668857958; //  Saturday, November 19, 2022 11:39:18 AM
-// Use https://www.epochconverter.com to convert (for now)
+    function toTimestamp(strDate){
+        let datum = Date.parse(strDate);
+        return datum/1000;
+    }
+
+    let startTimeInUnix = toTimestamp(startTime);
+    let endTimeInUnix =  toTimestamp(endTime);
+    console.log(startTimeInUnix);
+    console.log(endTimeInUnix);
+
+    let timeDifference = endTimeInUnix-startTimeInUnix;
+    console.log(timeDifference);
+    let numberOfHours = timeDifference/3600;
+    console.log(numberOfHours);
 
 
 
-let lat = 0.0;
-let lon = 0.0;
-let AQI = 0.0;
-let CO = 0.0;
-let NO = 0.0;
-let NO2 = 0.0;
-let OZONE = 0.0;
-let SO2 = 0.0;
-let PM25 = 0.0;
-let PM10 = 0.0;
-let NH3 = 0.0;
-
-
-function airQualityForHourByLatLon(inputLat,inputLon,startTime,endTime) {
-    let key = '725c1ea056aba9bb9f157aba9d00a3c2';
-
-    fetch('https://api.openweathermap.org/data/2.5/air_pollution/history?lat='+inputLat+'&lon='+inputLon+'&start='+startTime+'&end='+endTime+'&appid='+key)
-        .then(function(resp) { return resp.json() }) // Convert data to json
-        .then(function(data) {
-            let output = JSON.stringify(data)
-            console.log(output);
+    while(numberOfHours > 0){
+        airQualityForHourByLatLon(inputLat,inputLon,startTimeInUnix,startTimeInUnix+3600).then(jsonData => {
+            let output = JSON.stringify(jsonData);
             let array = output.split(",");
             for(let i = 0;i < array.length;i++){
-                console.log(array[i]);
+               // console.log(array[i]);
             }
-
-            lon = parseFloat(array[0].replace('{"coord":{"lon":', ""));
+            lon.push(parseFloat(array[0].replace('{"coord":{"lon":', "")));
             console.log(lon);
-            lat = parseFloat(array[1].slice(0,-1).replace('"lat":', ""));
+            lat.push(parseFloat(array[1].slice(0,-1).replace('"lat":', "")));
             console.log(lat);
-            AQI = parseFloat(array[2].slice(0,-1).replace('"list":[{"main":{"aqi":', ""));
+            AQI.push(parseFloat(array[2].slice(0,-1).replace('"list":[{"main":{"aqi":', "")));
             console.log(AQI);
-            CO = parseFloat(array[3].replace('"components":{"co":', ""));
+            CO.push(parseFloat(array[3].replace('"components":{"co":', "")));
             console.log(CO);
-            NO = parseFloat(array[4].replace('"no":', ""));
+            NO.push(parseFloat(array[4].replace('"no":', "")));
             console.log(NO);
-            NO2 = parseFloat(array[5].replace('"no2":', ""));
+            NO2.push(parseFloat(array[5].replace('"no2":', "")));
             console.log(NO2);
-            OZONE = parseFloat(array[6].replace('"o3":', ""));
+            OZONE.push(parseFloat(array[6].replace('"o3":', "")));
             console.log(OZONE);
-            SO2 = parseFloat(array[7].replace('"so2":', ""));
+            SO2.push(parseFloat(array[7].replace('"so2":', "")));
             console.log(SO2);
-            PM25 = parseFloat(array[8].replace('"pm2_5":', ""));
+            PM25.push(parseFloat(array[8].replace('"pm2_5":', "")));
             console.log(PM25);
-            PM10 = parseFloat(array[9].replace('"pm10":', ""));
+            PM10.push(parseFloat(array[9].replace('"pm10":', "")));
             console.log(PM10);
-            NH3 = parseFloat(array[10].replace('"nh3":', ""));
+            NH3.push(parseFloat(array[10].replace('"nh3":', "")));
             console.log(NH3);
 
-
-        })
-        .catch(function() {
-            // catch any errors
+            //Use values here in the promise
+            
+            
+            
         });
+        numberOfHours--;
+        startTimeInUnix = startTimeInUnix + 3600;
+    }
+
+    console.log("processEnd");
 }
 
-airQualityForHourByLatLon(inputLat,inputLon,startTime,endTime);
+
+async function airQualityForHourByLatLon(inputLat, inputLon, startTimeInput, endTimeInput) {
+    let key = '725c1ea056aba9bb9f157aba9d00a3c2';
+    let url = 'https://api.openweathermap.org/data/2.5/air_pollution/history?lat=' + inputLat + '&lon=' + inputLon + '&start=' + startTimeInput + '&end=' + endTimeInput + '&appid=' + key;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    return jsonData;
+}
+main();

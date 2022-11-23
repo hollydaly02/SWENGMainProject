b231ddadc18d47c95e08bee9f6caa61ffcf0432c
JamesOC3310 <98289189+JamesOC3310@users.noreply.github.com>
2022-11-23 23:47:46 +0000

    Added new page for dashboard, requires all html files to function properly

--- sweng-project-frontend/pages/index.tsx
+++ sweng-project-frontend/pages/index.tsx
@@ -1,10 +1,13 @@
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Dropdown from 'react-bootstrap/Dropdown';
-
+import Link from 'next/link'
 
 export default function Home() {
+  
+
   return (
     
     <div className={styles.container}>
@@ -17,40 +20,31 @@ export default function Home() {
     <div className={styles.dropbtn }>
       <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Area
+        Navigate to Dashboard
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Dublin</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">San Francisco</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Redmond</Dropdown.Item>
+        <Dropdown.Item href="Dashboard">Dashboard</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
     </div>
 
 
-
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to SWENG Group 15 Dashboard
+         </h1>
+         <h1 className={styles.title}>
+          Go to <Link href="Dashboard">Dashboard</Link>
+
         </h1>
+
+        
 
         
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      
     </div>
   )
 }

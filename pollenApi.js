5235613c3f48f21dc96c48c206761710c1d1863f
AndreyYamkovoy1 <100922899+AndreyYamkovoy1@users.noreply.github.com>
2022-11-23 16:01:33 +0000

    Update pollenApi.js
    Updated pollen to work so you can request historical for time period within 2 days.

--- pollenApi.js
+++ pollenApi.js
@@ -1,138 +1,70 @@
 const http = require("https");
 inputLat = 12.9889055;
 inputLng = 77.574044;
-inputDateFrom = "2020-07-13 00:00:00";
-inputDateTo = "2020-07-14 00:00:00";
-function latestByLatLng(inputLat,inputLng){
-    const options = {
-        "method": "GET",
-        "hostname": "api.ambeedata.com",
-        "port": null,
-        "path": `/latest/by-lat-lng?lat=${inputLat}&lng=${inputLng}`,
-        //"path": "/latest/by-lat-lng?lat=12.9889055&lng=77.574044",
-        "headers": {
-            "x-api-key": "5ab5633d25b8f8f48f14679fb0c78eeac59f0ee98b317f32122f091f782e1ac5",
-            "Content-type": "application/json"
-        }
-    };
-    let output = "";
-    let COString = "";
-    let CO = 0.0;
-    let NO2 = 0.0;
-    let OZONE = 0.0;
-    let PM10 = 0.0;
-    let PM25 = 0.0;
-    let SO2 = 0.0;
-    let city = "";
-    let countryCode = "";
-    let division = "";
-    let lat = 0.0;
-    let lng = 0.0;
-    let placeName = "";
-    let postalCode = "";
-    let state = "";
-    let updatedAt = "";
-    let pollutant = "";
-    let concentration = 0.0;
-    let category = "";
-    let AQI = 0.0;
-    const req = http.request(options, function (res) {
-        const chunks = [];
+//Input date/Time from latest 2 days.
+startTime= "2022-11-23 00:00:00";
+endTime = "2022-11-23 04:00:00";
 
-        res.on("data", function (chunk) {
-
-            chunks.push(chunk);
-
-
-
-        });
-
-        res.on("end", function () {
-            const body = Buffer.concat(chunks);
-            //console.log(body.toString());
-            output = body.toString();
-            newOutput = output.replace('{"message":"success","stations":[{',"");
-            newOutput2 = newOutput.replace(']}',"");
-            //newOutput = output.replace('}]',"");
-
-            array = newOutput2.split(",");
-            for(let i = 0;i < array.length;i++){
-                console.log(array[i]);
-
-            }
-
-            CO = parseFloat(array[0].replace('"CO":', ""));
-            console.log(CO);
-            NO2 = parseFloat(array[1].replace('"NO2":', ""));
-            console.log(NO2);
-            OZONE = parseFloat(array[2].replace('"OZONE":', ""));
-            console.log(OZONE);
-            PM10 = parseFloat(array[3].replace('"PM10":', ""));
-            console.log(PM10);
-            PM25 = parseFloat(array[4].replace('"PM25":', ""));
-            console.log(PM25);
-            SO2 = parseFloat(array[5].replace('"SO2":', ""));
-            console.log(SO2);
-            city = array[6].replace('"city":', "");
-            console.log(city);
-            countryCode = array[7].replace('"countryCode":', "");
-            console.log(countryCode);
-            division = array[8].replace('"division":', "");
-            console.log(division);
-            lat = parseFloat(array[9].replace('"lat":', ""));
-            console.log(lat);
-            lng = parseFloat(array[10].replace('"lng":', ""));
-            console.log(lng);
-            placeName = array[11].replace('"placeName":', "");
-            console.log(placeName);
-            postalCode = array[12].replace('"postalCode":', "");
-            console.log(postalCode);
-            state = array[13].replace('"state":', "");
-            console.log(state);
-            updatedAt = array[14].replace('"updatedAt":', "");
-            console.log(updatedAt);
-            pollutant = array[15].replace('"aqiInfo":{"pollutant":', "");
-            console.log(pollutant);
-            concentration = parseFloat(array[16].replace('"concentration":', ""));
-            console.log(concentration);
-            category = array[17].replace('"category":', "");
-            console.log(category);
-            AQI = parseFloat(array[18].slice(0,-1).replace('"AQI":', ""));
-            console.log(AQI);
-        });
-
-    });
-
-    req.end();
+function toTimestamp(strDate){
+    let datum = Date.parse(strDate);
+    return datum/1000;
 }
-function LatLngFromDateToDate(){
+let startTimeInUnix = toTimestamp(startTime);
+let endTimeInUnix =  toTimestamp(endTime);
+console.log(startTimeInUnix);
+console.log(endTimeInUnix);
 
+
+let timeDifference = endTimeInUnix-startTimeInUnix;
+console.log(timeDifference);
+let numberOfHours = timeDifference/3600;
+console.log(numberOfHours);
+
+let startDate = startTime[0]+startTime[1]+startTime[2]+startTime[3]+startTime[4]+startTime[5]+startTime[6]+startTime[7]+startTime[8]+startTime[9];
+//console.log(startDate);
+let endDate = endTime[0]+endTime[1]+endTime[2]+endTime[3]+endTime[4]+endTime[5]+endTime[6]+endTime[7]+endTime[8]+endTime[9];
+//console.log(endDate);
+let startHours = startTime[11]+startTime[12];
+//console.log(startHours);
+let startMin = startTime[14]+startTime[15];
+//console.log(startMin);
+let startSeconds = startTime[17]+startTime[18];
+//console.log(startSeconds);
+
+let endHours = endTime[11]+endTime[12];
+//console.log(endHours);
+let endMin = endTime[14]+endTime[15];
+//console.log(endMin);
+let endSeconds = endTime[17]+endTime[18];
+//console.log(endSeconds);
+
+function LatLngFromDateToDate(inputLat, inputLon, startDateInput,endDateInput,startHoursInput,startMinInput,startSecondsInput,endHoursInput,endMinInput,endSecondsInput){
+    let path = '/history/pollen/by-lat-lng?lat='+inputLat+'&lng=' + inputLon + '&from='+startDateInput +
+        '%20'+startHoursInput+'%3A'+startMinInput+'%3A'+startSecondsInput+ '&to=' +endDateInput +'%20'+
+        endHoursInput+'%3A'+endMinInput+'%3A'+endSecondsInput;
+
+    console.log(path);
     const options = {
         "method": "GET",
         "hostname": "api.ambeedata.com",
         "port": null,
-        //"path": "/history/by-lat-lng?lat=12.9889055&lng=77.574044&from=2020-07-13 12:16:44&to=2020-07-14 12:16:44",
-        "path": "/history/by-lat-lng?lat=12.9889055&lng=77.574044&from=2022-11-15%2000%3A00%3A00&to=2022-11-15%2001%3A00%3A00",
-        //"path": `/history/by-lat-lng?lat=${inputLat}&lng=${inputLng}&from=${inputDateFrom}&to=${inputDateTo}`,
+        "path": path,
         "headers": {
             "x-api-key": "5ab5633d25b8f8f48f14679fb0c78eeac59f0ee98b317f32122f091f782e1ac5",
             "Content-type": "application/json"
         }
     };
     let output = "";
-
-    let NO2 = 0.0;
-    let PM10 = 0.0;
-    let PM25 = 0.0;
-    let CO = 0.0;
-    let SO2 = 0.0;
-    let OZONE = 0.0;
-    let AQI = 0.0;
-    let lat = 0.0;
-    let lng = 0.0;
-    let createdAt = "";
-    let postalCode = "";
-    let majorPollutant = "";
+    let grassPollenCount = [];
+    let grassPollenRisk = [];
+    let treePollenCount = [];
+    let treePollenRisk = [];
+    let weedPollenCount = [];
+    let weedPollenRisk = [];
+    let lat = [];
+    let lng = [];
+    let time = [];
+    let createdAt = [];
     const req = http.request(options, function (res) {
         const chunks = [];
 
@@ -144,39 +76,60 @@ function LatLngFromDateToDate(){
             const body = Buffer.concat(chunks);
             console.log(body.toString());
             output = body.toString();
-            newOutput = output.replace('{"message":"success","data":[{',"");
+            newOutput = output.replace('{"message":"success",',"");
             newOutput2 = newOutput.replace('}]}',"");
-            //newOutput = output.replace('}]',"");
-            console.log(newOutput2);
+
+           // console.log(newOutput2);
             array = newOutput2.split(",");
             for(let i = 0;i < array.length;i++){
                 console.log(array[i]);
             }
+            for(let i = 0;i < numberOfHours;i++){
+                if(i == 0){
+                    lat.push(parseFloat(array[0].replace('"lat":', "")));
+                    //console.log(lat);
+                    lng.push(parseFloat(array[1].replace('"lng":', "")));
+                    //console.log(lng);
+                }
+               if( i == 0){
+                   grassPollenCount.push(parseFloat(array[(i*8)+2].replace('"data":[{"Count":{"grass_pollen":', "")));
 
-            CO = parseFloat(array[0].replace('"CO":', ""));
-            console.log(CO);
-            NO2 = parseFloat(array[1].replace('"NO2":', ""));
-            console.log(NO2);
-            OZONE = parseFloat(array[2].replace('"OZONE":', ""));
-            console.log(OZONE);
-            PM10 = parseFloat(array[3].replace('"PM10":', ""));
-            console.log(PM10);
-            PM25 = parseFloat(array[4].replace('"PM25":', ""));
-            console.log(PM25);
-            SO2 = parseFloat(array[5].replace('"SO2":', ""));
-            console.log(SO2);
-            lat = parseFloat(array[6].replace('"lat":', ""));
+                   //console.log(grassPollenCount);
+               }
+               else {
+                   grassPollenCount.push(parseFloat(array[(i*8)+2].replace('{"Count":{"grass_pollen":', "")));
+                  // console.log(grassPollenCount);
+               }
+                treePollenCount.push(parseFloat(array[(i*8)+3].replace('"tree_pollen":', "")));
+                //console.log(treePollenCount);
+                weedPollenCount.push(parseFloat(array[(i*8)+4].slice(0,-1).replace('"weed_pollen":', "")));
+                //console.log(weedPollenCount);
+                grassPollenRisk.push((array[(i*8)+5].replace('"Risk":{"grass_pollen":', "")));
+               // console.log(grassPollenRisk);
+                treePollenRisk.push((array[(i*8)+6].replace('"tree_pollen":', "")));
+               // console.log(treePollenRisk);
+                weedPollenRisk.push((array[(i*8)+7].slice(0,-1).replace('"weed_pollen":', "")));
+                //console.log(weedPollenRisk);
+                time.push(array[(i*8)+8].replace('"time":', ""));
+                if(i == numberOfHours-1 ){
+                    createdAt.push((array[(i*8)+9].replace('"createdAt":', "")));
+                }else{
+                    createdAt.push((array[(i*8)+9].slice(0,-1).replace('"createdAt":', "")));
+                }
+
+
+            }
             console.log(lat);
-            lng = parseFloat(array[7].replace('"lng":', ""));
             console.log(lng);
-            postalCode = array[8].replace('"postalCode":', "");
-            console.log(postalCode);
-            AQI = parseFloat(array[9].replace('"AQI":', ""));
-            console.log(AQI);
-            createdAt = array[10].replace('"createdAt":', "");
+            console.log(grassPollenCount);
+            console.log(treePollenCount);
+            console.log(weedPollenCount);
+            console.log(grassPollenRisk);
+            console.log(treePollenRisk);
+            console.log(weedPollenRisk);
+            console.log(time);
             console.log(createdAt);
-            majorPollutant = array[11].replace('"majorPollutant', "");
-            console.log(majorPollutant);
+
 
         });
 
@@ -184,6 +137,5 @@ function LatLngFromDateToDate(){
 
     req.end();
 }
-//latestByLatLng(inputLat,inputLng);
-//Change lat/lng and from one date by hour
-LatLngFromDateToDate();
+
+LatLngFromDateToDate(inputLat,inputLng,startDate,endDate,startHours,startMin,startSeconds,endHours,endMin,endSeconds);

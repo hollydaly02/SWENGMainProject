a4091214e6249cdc6bfffaa32b8e773a03102d84
Tom Moroney <moroneyt@tcd.ie>
2022-11-28 21:00:53 +0000

    Added multiple graphs to one page

--- main-project/src/App.js
+++ main-project/src/App.js
@@ -9,12 +9,17 @@ function App() {
   const [data] = useState([200, 250, 60, 150, 100, 175]);
   const svgRef = useRef();
 
-  useEffect(() => {
-    fetchData();
+  const createBarChart = async () => {
     // setting up svg container
     const w = 400;
     const h = 300;
-    const svg = d3.select(svgRef.current)
+
+    // Remove the old svg
+    d3.select('#barchart-container')
+    .select('svg')
+    .remove();
+
+    const svg = d3.select("#barchart-container").append("svg")
       .attr('width', w)
       .attr('height', h)
       .style('background', '#d3d3d3')
@@ -39,7 +44,7 @@ function App() {
       .ticks(5);
     svg.append('g')
       .call(xAxis)
-      .attr('transform', 'translate(0, ${h})'); // shift x-axis to bottom of bar chart
+      .attr("transform", "translate(0," + h + ")") // shift x-axis to bottom of bar chart
     svg.append('g')
       .call(yAxis);
 
@@ -51,7 +56,135 @@ function App() {
         .attr('y', yScale) // sets up y scale with yScale function
         .attr('width', xScale.bandwidth())
         .attr('height', val => h - yScale(val)); // makes height of bar
+  }
 
+  const createLineChart = async () => {
+    // read data from csv and format variables
+    let data = await d3.csv('https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv')
+    var parseTime = d3.timeParse("%Y-%m-%d");
+  
+    data.forEach((d) => {
+      d.date = parseTime(d.date);
+      d.value = +d.value;
+    });
+    console.log(data)
+
+    // set the dimensions and margins of the graph
+    var margin = { top: 50, right: 20, bottom: 50, left: 70 },
+    width = 400 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+    // Remove the old svg
+    d3.select('#linechart-container')
+      .select('svg')
+      .remove();
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#linechart-container").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // add X axis and Y axis
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    x.domain(d3.extent(data, (d) => { return d.date; }));
+    y.domain([0, d3.max(data, (d) => { return d.value; })]);
+  
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
+
+    svg.append("g")
+      .call(d3.axisLeft(y));
+      
+    // add the Line
+    var valueLine = d3.line()
+    .x((d) => { return x(d.date); })
+    .y((d) => { return y(d.value); });
+  
+    svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", valueLine);
+  }
+
+  const createPieChart = async () => {
+    const data = [{ label: 'Rainy', value: 10 }, { label: 'Sunny', value: 20 }, { label: 'Cloudy', value: 30 }];
+    const outerRadius = 100;
+    const innerRadius = 0;
+    const margin = {
+      top: 50, right: 50, bottom: 50, left: 50,
+    };
+
+    const width = 2 * outerRadius + margin.left + margin.right;
+    const height = 2 * outerRadius + margin.top + margin.bottom;
+
+    const colorScale = d3     
+      .scaleSequential()      
+      .interpolator(d3.interpolateCool)      
+      .domain([0, data.length]);
+
+    // Remove the old svg
+    d3.select('#pie-container')
+      .select('svg')
+      .remove();
+
+    // Create new svg
+    const svg = d3
+      .select('#pie-container')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+    const arcGenerator = d3
+      .arc()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius);
+
+    const pieGenerator = d3
+      .pie()
+      .padAngle(0)
+      .value((d) => d.value);
+
+    const arc = svg
+      .selectAll()
+      .data(pieGenerator(data))
+      .enter();
+
+    // Append arcs
+    arc
+      .append('path')
+      .attr('d', arcGenerator)
+      .style('fill', (_, i) => colorScale(i))
+      .style('stroke', '#ffffff')
+      .style('stroke-width', 0);
+
+    // Append text labels
+    arc
+      .append('text')
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle')
+      .text((d) => d.data.label)
+      .style('fill', (_, i) => colorScale(data.length - i))
+      .attr('transform', (d) => {
+        const [x, y] = arcGenerator.centroid(d);
+        return `translate(${x}, ${y})`;
+      });
+    
+  } 
+
+  useEffect(() => {
+    createBarChart();
+    createLineChart();
+    createPieChart();
   }, []);
 
   const fetchData = async () => {
@@ -67,41 +200,15 @@ function App() {
   };
 
   return (
-    <div className="homepage">
-      <div className="header">
-        <h1>Testing Users API</h1>
-      </div>
-      <svg ref={svgRef}></svg>
-      <div className="searchbar">
-        <input
-          type="text"
-          placeholder="Search for User"
-          onChange={(e) => setGithubUser(e.target.value)}
-          className="input_search"
-        />
-        <button onClick={fetchData} className="search_button">
-          Search Github
-        </button>
-      </div>
-      <br></br>
-      <div className="mainBody">
-        <img src={githubData.avatar_url} height="100" width="100" />
-        <br></br>
-        <p>
-          <span>Username:</span> {githubData.name}
-        </p>
-        <p>
-          <span>Location:</span> {myfunc(githubData.location)}
-        </p>
-        <p>
-          <span>Public Repos:</span> {githubData.public_repos}
-        </p>
-      </div>
+    <div>
+      <div id="barchart-container" />
+      <div id="linechart-container" />
+      <div id="pie-container" />
     </div>
   );
 }
 
-function myfunc( location){
+function myfunc(location){
   if(location) {
     if(location.indexOf("San Francisco") !== -1){
       return "San Francisco"

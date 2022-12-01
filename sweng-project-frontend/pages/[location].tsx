daefe071b6313e9f537f10f7e6b58e7c592b7fed
Liam Junkermann <dev@ljsoft.xyz>
2022-12-01 12:58:16 +0000

    convert pages for each location into single file

---
+++ sweng-project-frontend/pages/[location].tsx
@@ -0,0 +1,50 @@
+import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from "next/types";
+import styles from "../styles/Dashboard.module.css";
+export default function LocationView({ location }: { location: string }) {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.overimg1}>
+          <form action="/action_page.php" className={styles.form}>
+            <div className={styles.inputcontainer}>
+              <i className="fa fa-calendar icon"></i>
+              <input
+                className={styles.inputfield}
+                type="date"
+                placeholder="Password"
+                name="pswrd"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className={styles.btn}
+              //onClick="check(this.form)"
+              value="Login"
+            >
+              Input
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const locations = ["Dublin", "Redmond", "SanFrancisco", "NewDelhi"];
+  return {
+    paths: locations.map((location) => ({ params: { location: location } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<{ location: string }> = ({
+  params,
+}) => {
+  if (params && params.location && typeof params.location === "string") {
+    return { props: { location: params.location } };
+  }
+  throw new Error("getStaticPropsFailed");
+};

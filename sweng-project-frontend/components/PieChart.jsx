4ca0aae22d3d71a959aefa97f745e3fe848a541f
Liam Junkermann <dev@ljsoft.xyz>
2022-12-02 20:14:03 +0000

    d3 bug fixes

--- sweng-project-frontend/components/PieChart.jsx
+++ sweng-project-frontend/components/PieChart.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import d3 from 'd3';
+import React from "react";
+import * as d3 from "d3";
 
+const Chart = () => {
+  const svg = React.useRef(null);
+  React.useEffect(() => {
+    drawChart(svg);
+  }, [svg]);
 
-function drawChart(svgRef) {
-  const data = [
-    { label: "Rainy", value: 10 },
-    { label: "Sunny", value: 20 },
-    { label: "Cloudy", value: 30 },
-  ];
   const outerRadius = 200;
   const innerRadius = 0;
   const margin = {
@@ -20,66 +19,71 @@ function drawChart(svgRef) {
   const width = 2 * outerRadius + margin.left + margin.right;
   const height = 2 * outerRadius + margin.top + margin.bottom;
 
-  const colorScale = d3
-    .scaleSequential()
-    .interpolator(d3.interpolateCool)
-    .domain([0, data.length]);
+  function drawChart(svgRef) {
+    const data = [
+      { label: "Rainy", value: 10 },
+      { label: "Sunny", value: 20 },
+      { label: "Cloudy", value: 30 },
+    ];
 
-  // Create new svg
-  const svg = d3
-    .select(svgRef.current)
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", `translate(${width / 2}, ${height / 2})`);
+    const colorScale = d3
+      .scaleSequential()
+      .interpolator(d3.interpolateCool)
+      .domain([0, data.length]);
 
-  const arcGenerator = d3
-    .arc()
-    .innerRadius(innerRadius)
-    .outerRadius(outerRadius);
+    // Create new svg
+    const svg = d3
+      .select(svgRef.current)
+      .attr(
+        "viewbox",
+        `0 0 ${height + margin.top + margin.bottom} ${
+          width + margin.left + margin.right
+        }`
+      )
+      .append("g")
+      .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
-  const pieGenerator = d3
-    .pie()
-    .padAngle(0)
-    .value((d) => d.valueOf());
+    const arcGenerator = d3
+      .arc()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius);
 
-  const arc = svg
-    .selectAll()
-    .data(pieGenerator(data.map((d) => d.value)))
-    .enter();
+    const pieGenerator = d3
+      .pie()
+      .padAngle(0)
+      .value((d) => d.valueOf());
 
-  // Append arcs
-  arc
-    .append("path")
-    .attr("d", arcGenerator)
-    .style("fill", (_, i) => colorScale(i))
-    .style("stroke", "#ffffff")
-    .style("stroke-width", 0);
+    const arc = svg
+      .selectAll()
+      .data(pieGenerator(data.map((d) => d.value)))
+      .enter();
 
-  // Append text labels
-  arc
-    .append("text")
-    .attr("text-anchor", "middle")
-    .attr("alignment-baseline", "middle")
-    .text((d) => d.value)
-    .style("fill", (_, i) => colorScale(data.length - i))
-    .attr("transform", (d) => {
-      const [x, y] = arcGenerator.centroid(d);
-      return `translate(${x}, ${y})`;
-    });
-}
+    // Append arcs
+    arc
+      .append("path")
+      .attr("d", arcGenerator)
+      .style("fill", (_, i) => colorScale(i))
+      .style("stroke", "#ffffff")
+      .style("stroke-width", 0);
 
-const Chart = () => {
-  const svg = React.useRef(null);
-  React.useEffect(() => {
-    drawChart(svg);
-  }, [svg]);
+    // Append text labels
+    arc
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .text((d) => d.value)
+      .style("fill", (_, i) => colorScale(data.length - i))
+      .attr("transform", (d) => {
+        const [x, y] = arcGenerator.centroid(d);
+        return `translate(${x}, ${y})`;
+      });
+  }
 
   return (
-    <div id="chart">
-      <svg ref={svg} />
-    </div>
+    // <div id="chart" ref={svg} />
+    // <div id="chart">
+    <svg width={width} height={height} ref={svg} />
+    // </div>
   );
 };
 
